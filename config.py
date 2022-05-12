50b169e91812d5bf4c5e3688dd0142416584c8fe
Aliyo254 <alinurali254@gmail.com>
2022-05-12 14:20:10 +0300

    App is ruuning

--- config.py
+++ config.py
@@ -1,25 +1,31 @@
 import os
 
+
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    '''
+    class config
+    '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-
 class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     pass
 
 
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
-
 }
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='1234'
+
+
+python3.10 manage.py server
