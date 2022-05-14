44d7e50a1e4050552b938255c57c9c6342febc63
Aliyo254 <alinurali254@gmail.com>
2022-05-14 11:11:44 +0300

    Profile and gunicorn install

--- config.py
+++ config.py
@@ -1,36 +1,46 @@
 import os
 
-
 class Config:
 
     '''
     class config
     '''
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alinur:admin@localhost/pitch'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
- #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    
 
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USE_SSL = True
+    MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
+
+
+
+
+
+
+
+
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alinur:admin@localhost/pitch_test'
 
 class DevConfig(Config):
     '''
     Development  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -39,5 +49,6 @@ class DevConfig(Config):
 
 config_options = {
 'development':DevConfig,
+'test':TestConfig,
 'production':ProdConfig
 }
