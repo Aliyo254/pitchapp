2d11ffdc5e27fdec647aff256456348b09d6c848
Aliyo254 <alinurali254@gmail.com>
2022-05-14 11:52:49 +0300

    initial commit

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alinur:admin@localhost/pitch'
     DEBUG = True
 
 config_options = {
